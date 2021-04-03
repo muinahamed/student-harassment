ef3d739e859e37f70e7cf4ab6c89362fc14ae61b
muin <muinahamed1234@gmail.com>
2021-04-03 20:48:54 +0600

    added postgres database configuration

--- config/database.php
+++ config/database.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
+$DATABASE_URL = parse_url('//muadyiocgieiue:e540a851f5bb9011e9510c6077cb674f1dd6dba2e203363ccb395e3326eaadc4@ec2-52-44-31-100.compute-1.amazonaws.com:5432/da');
 
 return [
 
@@ -63,19 +64,17 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => [
+         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
